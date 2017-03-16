99f8bd16180526dddec76337f1750132e824008a
king.wang <wdxxlanswer@gmail.com>
2017-03-16 10:52:57 +0800

    Add Hello Java (Converted to .h & .m files)

--- J2Objc_OC_Version2/ViewController.m
+++ J2Objc_OC_Version2/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Hello.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [TestHello mark];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
